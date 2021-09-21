9e724ee85cc4d99ae6ba612979127bd7be34d520
Abir Taheer <abir@taheer.me>
2021-09-21 13:51:22 -0400

    added backdrop

---
+++ comps/ui/Backdrop.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react";
+import useScript from "../../hooks/useScript";
+import styles from "./Backdrop.module.css";
+
+export default function Backdrop() {
+  const ref = useRef();
+  const threeJS = useScript(
+    "https://cdnjs.cloudflare.com/ajax/libs/three.js/r119/three.min.js"
+  );
+  const vantaDots = useScript(
+    "https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.dots.min.js"
+  );
+
+  useEffect(() => {
+    if (threeJS === "ready" && vantaDots === "ready" && ref.current) {
+      globalThis.VANTA.DOTS({
+        el: ref.current,
+        mouseControls: false,
+        touchControls: false,
+        gyroControls: false,
+        minHeight: 600,
+        minWidth: 800,
+        scale: 1.0,
+        scaleMobile: 1.0,
+        color: 0xffffff,
+        color2: 0xffffff,
+        backgroundColor: 0x6c5ce7,
+      });
+    }
+  }, [ref, vantaDots, threeJS]);
+
+  return <div ref={ref} className={styles.backdrop} />;
+}
---
+++ comps/ui/Footer.js
@@ -0,0 +1,19 @@
+import styles from "./Footer.module.css";
+import Link from "@mui/material/Link";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        View Source on{" "}
+        <Link
+          href={"https://github.com/abir-taheer"}
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+        >
+          GitHub
+        </Link>
+      </p>
+    </footer>
+  );
+}
