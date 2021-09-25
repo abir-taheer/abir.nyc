a5ef9a07b78c55dd202e74fbaa1408d043f26bd8
Abir Taheer <abir@taheer.me>
2021-09-24 23:26:11 -0400

    removed slide from experience section because of client rendering issues

--- comps/experience/Experience.js
+++ comps/experience/Experience.js
@@ -1,39 +1,32 @@
-import { Box, Tabs, Tab, Slide } from "@mui/material";
 import { useRef, useState } from "react";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 export default function Experience() {
   const [tab, setTab] = useState("stuysu");
-  const ref = useRef();
 
   return (
-    <Slide
-      direction="up"
-      in={!!ref.current}
-      ref={ref}
-      mountOnEnter
-      unmountOnExit
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: 224,
+      }}
     >
-      <Box
-        sx={{
-          flexGrow: 1,
-          bgcolor: "background.paper",
-          display: "flex",
-          height: 224,
-        }}
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={tab}
+        onChange={(_, t) => setTab(t)}
+        aria-label="Organizations"
+        sx={{ borderRight: 1, borderColor: "divider" }}
       >
-        <Tabs
-          orientation="vertical"
-          variant="scrollable"
-          value={tab}
-          onChange={(_, t) => setTab(t)}
-          aria-label="Organizations"
-          sx={{ borderRight: 1, borderColor: "divider" }}
-        >
-          <Tab label="Stuyvesant Student Union" value={"stuysu"} />
-          <Tab label="BlockChains for Schools" value={"bfs"} />
-          <Tab label="Stuyvesant Board of Elections" value={"stuyboe"} />
-        </Tabs>
-      </Box>
-    </Slide>
+        <Tab label="Stuyvesant Student Union" value={"stuysu"} />
+        <Tab label="BlockChains for Schools" value={"bfs"} />
+        <Tab label="Stuyvesant Board of Elections" value={"stuyboe"} />
+      </Tabs>
+    </Box>
   );
 }
